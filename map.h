70fc4e5bcc1d86d43acfe146dd0dd5b771b3601a
MTien-Vn <manhtien2022000@gmail.com>
2021-11-07 22:22:06 +0700

    add module create map

---
+++ map.h
@@ -0,0 +1,25 @@
+#include <vector>
+
+#ifndef __MAP_H__
+#define __MAP_H__
+
+#define EMPTY_MAP_ERROR -1
+#define CREATE_MAP_ERROR 0
+#define CREATE_MAP_SUCCESS 1
+
+using namespace std;
+
+struct Node
+{
+    int coX;
+    int coY;
+    double distance;
+    double time;
+    int id;
+};
+
+int createMap(char *fileName, vector<Node> map[]);
+void printMap(vector<Node> map[], int numberNode);
+int countNumberNodeInMap(char fileName[]);
+
+#endif
